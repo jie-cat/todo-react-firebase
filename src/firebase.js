1edc19f9397d21f6e092fd008cfa839f975f5d15
jei-cat <105295892+jie-cat@users.noreply.github.com>
2022-11-14 19:41:35 +0800

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -12,13 +12,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DIMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
-  measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAzXZSqYzhogcb-BL-AGXllBm4BxPujONs",
+  authDomain: "todo2-app-8770f.firebaseapp.com",
+  projectId: "todo2-app-8770f",
+  storageBucket: "todo2-app-8770f.appspot.com",
+  messagingSenderId: "149188483030",
+  appId: "1:149188483030:web:011a78cf236915ca68f677",
+  measurementId: "G-0LY7X4LHNB",
 };
 
 // Initialize Firebase
